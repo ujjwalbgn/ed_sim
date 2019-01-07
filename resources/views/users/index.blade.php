7ee1eb8f04f1f30e1b85ae14977c4b1b2fd12088
Sarthak_Ratna_Sthapit <sarthak.r.stha@gmail.com>
2019-01-07 23:57:01 +0545

    css

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,16 +3,21 @@
 @extends('layout.adminMenu')
 
 @section('content')
-    <div class="container">
+<div class="container">
+    <div class="row justify-content-center">
         <div class="content">
-            <h1 class="title">All Users</h1>
+            <h1 class="title">All Users</h1> 
+            <p>
             <a class="button is-link" href="/register">
                 <strong>Register New User</strong>
             </a>
+           
             <a class="button is-link" href="/roles">
                 <strong>Manage Roles</strong>
             </a>
+            </p><br>
             @include('users.userTabel')
         </div>
     </div>
+</div>
 @endsection
