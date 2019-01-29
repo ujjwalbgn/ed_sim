f856bb5056a10ed4a9c7b78d50f20ea4b30ca0ef
Ujjwal Bajagain <ubajagain@gmail.com>
2019-01-28 18:51:39 -0600

    signature relation added to patient

---
+++ resources/views/patients/showPatientSignature.blade.php
@@ -0,0 +1,55 @@
+            <hr>
+            <div class="columns">
+                <div class="column">
+                <h4 class="title is-4">Signature Assigned</h4>
+                </div>
+                <div class="column">
+                <a class="button is-link" href="/patients/{{$patient->id}}/signatures">
+                <strong>Edit Patient's Records Signature</strong>
+            </a>
+                    @if(isset($signatures))
+                </div></div>
+            <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+                <thead>
+                <tr>
+                    <th>
+                        No.
+                    </th>
+                    <th>
+                        Print Name
+                    </th>
+                    {{--<th>--}}
+                        {{--Time--}}
+                    {{--</th>--}}
+                    {{--<th>Description</th>--}}
+                    <th>
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($signatures as $index => $signature)
+                    <tr>
+                        <th>{{$index+1}}</th>{{-- (+1) beacuse $index starts from 0 --}}
+                    <th><a href="/signatures/{{$signature->id}}">
+                            {{$signature->printName}}
+                        </a></th>
+                        {{--<th>--}}
+                            {{--{{$signature->time}}--}}
+                        {{--</th>--}}
+                        {{--<th>--}}
+                            {{--{{$signature->description}}--}}
+                        {{--</th>--}}
+                        <th>
+                        <form method="post" action="/patients/{{$patient->id}}/signature/{{$signature->id}}">
+                                @method('DELETE')
+                                @csrf
+                                        <button type="submit" class="button is-danger is-outlined">Remove signatureication </button>
+                            </form></th>
+                </tr>
+                </tbody>
+                @endforeach
+            </table>
+@else
+    <br>
+    No Signature Found
+@endif
